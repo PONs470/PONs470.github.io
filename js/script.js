c91b94fb40409c4a8f0ce2e443240e87ad86a229
shoki <shouki_25n1200544@nnn.ed.jp>
2025-09-18 13:33:12 +0900

    test8

--- js/script.js
+++ js/script.js
@@ -8,3 +8,44 @@ window.addEventListener("load", () => {
         loading.classList.add("loaded");
     }, 3000);
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var main = new Splide('#main-carousel', {
+        type: 'fade',
+        rewind: true,
+        pagination: false,
+        arrows: false,
+        cover: true,
+        heightRatio: 0.5,
+        autoplay: {
+            speed: 3,
+        },
+    });
+
+    var thumbnails = new Splide('#thumbnail-carousel', {
+        fixedWidth: 100,
+        fixedHeight: 60,
+        gap: 10,
+        rewind: true,
+        pagination: false,
+        cover: true,
+        isNavigation: true,
+        focus: 'center',
+        breakpoints: {
+            600: {
+                fixedWidth: 60,
+                fixedHeight: 44,
+            },
+        },
+        autoplay: {
+            speed: 5,
+        },
+        arrows: false,
+    });
+
+    main.sync(thumbnails);
+    main.mount();
+    thumbnails.mount();
+
+});
